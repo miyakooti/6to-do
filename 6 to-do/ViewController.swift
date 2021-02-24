be9f9363e59df8bb62cdddc4ca30209b727a3d75
arai <niimaru09@gmail.com>
2021-02-24 22:11:55 +0900

    Initial Commit

---
+++ 6 to-do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6 to-do
+//
+//  Created by Arai Kousuke on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
