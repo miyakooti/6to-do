70bed12c4a02a2e398f63fde8e94d42eb9c99389
arai <niimaru09@gmail.com>
2021-03-24 18:37:22 +0900

    constraintsの作成、リネーム

--- 6 to-do/C/TopViewController.swift
+++ 6 to-do/C/TopViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import GoogleMobileAds
 
-class ViewController: UIViewController {
+class TopViewController: UIViewController {
     
     @IBOutlet weak var bannerView: GADBannerView!
 
@@ -10,6 +10,67 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //見た目の設定いろいろ
+        setUpView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // SeeViewControllerまたはInputViewControllerから遷移してきたときには、自動的に他のVCに遷移する。
+        if UserDefaults.standard.object(forKey: "fromSeeVC") != nil {
+            UserDefaults.standard.removeObject(forKey: "fromSeeVC")
+            performSegue(withIdentifier: "showInputVC", sender: nil)
+        }
+        if UserDefaults.standard.object(forKey: "fromInputVC") != nil {
+            UserDefaults.standard.removeObject(forKey: "fromInputVC")
+            performSegue(withIdentifier: "showSeeVC", sender: nil)
+        }
+        
+    }
+
+    @IBAction func tapStart(_ sender: Any) {
+        if UserDefaults.standard.object(forKey: "sixTaskList") == nil{
+            let alertController = UIAlertController(title: "まだタスクを登録していません。", message: "新たにタスクを登録しますか？", preferredStyle: .actionSheet)
+            let action1 = UIAlertAction(title: "はい", style: .default) { (alert) in
+                self.performSegue(withIdentifier: "showInputVC", sender: nil)
+            }
+            let action2 = UIAlertAction(title: "いいえ", style: .cancel)
+            
+            alertController.addAction(action1)
+            alertController.addAction(action2)
+            
+            // iPad対策--------------------------------------------------
+            alertController.popoverPresentationController?.sourceView=self.view
+            let screenSize = UIScreen.main.bounds
+            // ここで表示位置を調整
+            // xは画面中央、yは画面下部になる様に指定
+            alertController.popoverPresentationController?.sourceRect=CGRect(x:screenSize.size.width/2,y:screenSize.size.height,width:0,height:0)
+            // /iPad対策--------------------------------------------------
+            
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: "showSeeVC", sender: nil)
+        }
+    }
+    
+    @objc func tapSetting(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "showSettingTVC", sender: nil)
+    }
+    
+    func checkSegueSetting() {
+        if UserDefaults.standard.object(forKey: "goToShowSetting") != nil && UserDefaults.standard.object(forKey: "sixTaskList") != nil {
+            let goToShowSetting = UserDefaults.standard.object(forKey: "goToShowSetting") as! Bool
+            switch goToShowSetting {
+            case true:
+                performSegue(withIdentifier: "showSeeVC", sender: nil)
+            case false:
+                return
+            }
+        }
+    }
+    
+    func setUpView(){
         self.overrideUserInterfaceStyle = .light
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.20, green: 0.23, blue: 0.36, alpha: 1.0)
         startButton.layer.cornerRadius = 5
@@ -30,7 +91,7 @@ class ViewController: UIViewController {
         
         startButton.layer.shadowColor = UIColor.black.cgColor
         startButton.layer.shadowRadius = 5
-        startButton.layer.shadowOffset = CGSize(width: 0, height: 0)
+        startButton.layer.shadowOffset = .zero
         startButton.layer.shadowOpacity = 0.3
         
         // GADBannerViewのプロパティを設定
@@ -39,63 +100,9 @@ class ViewController: UIViewController {
 
         // 広告読み込み
         bannerView.load(GADRequest())
-        
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // SeeViewControllerまたはInputViewControllerから遷移してきたときには、自動的に他のVCに遷移する。
-        if UserDefaults.standard.object(forKey: "fromSeeVC") != nil {
-            UserDefaults.standard.removeObject(forKey: "fromSeeVC")
-            performSegue(withIdentifier: "input", sender: nil)
-        }
-        if UserDefaults.standard.object(forKey: "fromInputVC") != nil {
-            UserDefaults.standard.removeObject(forKey: "fromInputVC")
-            performSegue(withIdentifier: "show", sender: nil)
-        }
-        
-    }
-
-    @IBAction func tapStart(_ sender: Any) {
-        if UserDefaults.standard.object(forKey: "sixTaskList") == nil{
-            let alertController = UIAlertController(title: "まだタスクを登録していません。", message: "新たにタスクを登録しますか？", preferredStyle: .actionSheet)
-            let action1 = UIAlertAction(title: "はい", style: .default) { (alert) in
-                self.performSegue(withIdentifier: "input", sender: nil)
-            }
-            let action2 = UIAlertAction(title: "いいえ", style: .cancel)
-            
-            alertController.addAction(action1)
-            alertController.addAction(action2)
-            
-            // iPad対策--------------------------------------------------
-            alertController.popoverPresentationController?.sourceView=self.view
-            let screenSize = UIScreen.main.bounds
-            // ここで表示位置を調整
-            // xは画面中央、yは画面下部になる様に指定
-            alertController.popoverPresentationController?.sourceRect=CGRect(x:screenSize.size.width/2,y:screenSize.size.height,width:0,height:0)
-            // /iPad対策--------------------------------------------------
-            
-            self.present(alertController, animated: true, completion: nil)
-        } else {
-            performSegue(withIdentifier: "show", sender: nil)
-        }
-    }
-    
-    @objc func tapSetting(_ sender: UIBarButtonItem) {
-        performSegue(withIdentifier: "setting", sender: nil)
-    }
-    
-    func checkSegueSetting() {
-        if UserDefaults.standard.object(forKey: "goToShowSetting") != nil && UserDefaults.standard.object(forKey: "sixTaskList") != nil {
-            let goToShowSetting = UserDefaults.standard.object(forKey: "goToShowSetting") as! Bool
-            switch goToShowSetting {
-            case true:
-                performSegue(withIdentifier: "show", sender: nil)
-            case false:
-                return
-            }
-        }
-    }
 }
+
+
 
---
+++ 6 to-do/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  6 to-do
+//
+//  Created by Arai Kousuke on 2021/03/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let encourageMessageList:[String] = ["その調子です。","毎日お疲れさまです。","良い一日になりますように。","タスクを楽しみましょう。","いつでもあなたらしく。","ずっと応援しています。","疲れたら休憩しましょう。","アプリを使ってくれてありがとう。", "目を覚ますなら、朝シャンがおすすめです。", "目を使いすぎると、頭が凝るみたいです。", "今日いいことはありましたか？", "Eigowoshabereruyouni Naritaina.", "毎日の温かいお風呂に感謝。"]
+}
