9f959236c886ffe274f0687e6e7250145325ae72
miyakooti <niimaru09@gmail.com>
2021-09-10 01:20:40 +0900

    add colorPick base

---
+++ 6 to-do/Classes/Modules/Setting/ColorPick/ColorPickViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ColorPickViewController.swift
+//  6 to-do
+//
+//  Created by Arai Kousuke on 2021/09/10.
+//
+
+import UIKit
+
+class ColorPickViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
