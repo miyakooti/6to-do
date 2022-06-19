dc8ca3a81a0a1ed269fe073a4ff01f0b6f00da11
arai <niimaru09@gmail.com>
2022-06-19 18:24:46 +0900

    handle ios15

--- 6 to-do/Classes/Modules/Top/TopViewController.swift
+++ 6 to-do/Classes/Modules/Top/TopViewController.swift
@@ -98,6 +98,17 @@ final class TopViewController: UIViewController {
         self.navigationItem.title = VCType.top.navigationTitle
         self.navigationController!.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         
+        if #available(iOS 15.0, *) {
+             let appearance = UINavigationBarAppearance()
+             appearance.configureWithOpaqueBackground()
+             // NavigationBarの背景色の設定
+             appearance.backgroundColor = UIColor.init(hex: "525672")
+             // NavigationBarのタイトルの文字色の設定
+             appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+             self.navigationController?.navigationBar.standardAppearance = appearance
+             self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+         }
+        
         settingButton = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(tapSetting(_:)))
         self.navigationItem.rightBarButtonItem = settingButton
         
